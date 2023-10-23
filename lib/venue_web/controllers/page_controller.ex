a86da4fb003feeab1a310e7dc579ad8615d39c06
Chris Nguyen <chris.dungnguyen@gmail.com>
2023-10-23 16:04:47 +0700

    feat: load home page

--- lib/venue_web/controllers/page_controller.ex
+++ lib/venue_web/controllers/page_controller.ex
@@ -1,9 +1,14 @@
 defmodule VenueWeb.PageController do
   use VenueWeb, :controller
 
+  alias Venue.Feeds
+
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, invite: [], activities: [], layout: false)
+    activities = Feeds.list_activities(conn)
+    invite = Feeds.list_invite(conn)
+
+    render(conn, :home, invite: invite, activities: activities, layout: false)
   end
 end
